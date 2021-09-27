b6676e4e25ae031279206e4653f03b66ba9cb170
Gary <abc77896@gmaiil.com>
2021-09-27 15:45:35 +0800

    新增 loading code

--- train_resnet50.py
+++ train_resnet50.py
@@ -18,7 +18,7 @@ for gpu in gpus:
 DATASET_PATH  = 'C:/Users/user/Desktop/學校/109-2/巨量課程2/Data/image_data/'
 IMAGE_SIZE = (224, 224)
 NUM_CLASSES = 5
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 NUM_EPOCHS = 10
 WEIGHTS_FINAL = 'model-resnet50-final.h5'
 
@@ -54,13 +54,13 @@ rest_layer = ResNet50(include_top=False, weights='imagenet',
 
 Res_model = Sequential()
 Res_model.add(rest_layer)
+Res_model.layers[0].trainable = False
 Res_model.add(Flatten())
 Res_model.add(Dropout(0.5))
 Res_model.add(Dense(5, activation='softmax', name='prediction'))
 
 Res_model.compile(optimizer=Adam(learning_rate=1e-5),
                   loss='categorical_crossentropy', metrics=['accuracy'])
-
 # print(net_final.summary())
 best_model_path = 'TL_best_model.h5'
 checkpoint_callback = ModelCheckpoint(
@@ -73,11 +73,9 @@ history_res = Res_model.fit_generator(train_batches,
                             steps_per_epoch = train_batches.samples // BATCH_SIZE,
                             validation_data = valid_batches,
                             validation_steps = valid_batches.samples // BATCH_SIZE,
-                            callbacks=[best_model_path],
+                            callbacks=[checkpoint_callback],
                             epochs = NUM_EPOCHS)
-
 history_dict = history_res.history
-
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
 epochs = range(1, len(loss_values) + 1)
@@ -102,4 +100,35 @@ print('time elapsed: ' + str(round(t2-t1, 2)) + ' seconds')
 
 Res_model.load_weights(best_model_path) 
 Res_model.save(WEIGHTS_FINAL)
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import glob
+
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+file_path = 'C:/Users/user/Desktop/學校/109-2/巨量課程2/Data/image_data/test/'
+f_names = glob.glob(file_path + '*.jpg')
+
+net = load_model('model-resnet50-final.h5') #load model
+# net = load_model('model-VGG16-final.h5')
+# net = load_model('model-ResNet101V2-final.h5')
+
+cls_list = ['daisy', 'dandelion','rose','sunflower','tulip']
+
+#prediction
+for f in range(10): 
+    img = image.load_img(f_names[f], target_size=(224, 224)) 
+    x = image.img_to_array(img)  
+    x = np.expand_dims(x, axis=0)   
+    pred = net.predict(x)[0]
+    top_inds = pred.argsort()[::-1]
+    print(f_names[f])
+    for i in top_inds:
+        print('{:.3f}  {}'.format(pred[i], cls_list[i]))
 
